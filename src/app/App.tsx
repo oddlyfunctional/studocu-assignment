c17e8b1435b309b307839649db5749bd8145593a
Marcos Rodrigues <mrodrigues.uff@gmail.com>
2024-03-21 00:17:43 +0100

    Remove questions

---
+++ src/app/App.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { NewQnA } from "@/app/components/NewQnA/NewQnA";
+import { QnAList } from "@/app/components/QnAList/QnAList";
+import type { NonEmptyString, QnA } from "@/domain/core";
+import { useState } from "react";
+
+const compareStrings = (a: string, b: string) => {
+  if (a < b) return -1;
+  if (a > b) return 1;
+  return 0;
+};
+
+export const App = () => {
+  const [items, setItems] = useState<QnA[]>([
+    {
+      id: "blankslate",
+      question: "How to add a question?" as NonEmptyString,
+      answer: "Just use the form below!" as NonEmptyString,
+      createdAt: new Date(),
+    },
+  ]);
+  const addNewQnA = (item: QnA) => setItems((items) => [...items, item]);
+  const sortQnAs = () =>
+    setItems((items) =>
+      [...items].sort((a, b) => compareStrings(a.question, b.question))
+    );
+
+  return (
+    <main>
+      <h1>The awesome Q/A tool</h1>
+
+      <div>
+        <h2>Created questions</h2>
+        <QnAList items={items} />
+        <button onClick={sortQnAs}>Sort questions</button>
+        <button onClick={() => setItems([])}>Remove questions</button>
+      </div>
+
+      <NewQnA onSubmit={addNewQnA} />
+    </main>
+  );
+};
--- src/app/components/QnAList/QnAList.tsx
+++ src/app/components/QnAList/QnAList.tsx
@@ -2,6 +2,10 @@ import { QnAItem } from "@/app/components/QnAList/QnAItem";
 import type { QnA } from "@/domain/core";
 
 export const QnAList = ({ items }: { items: QnA[] }) => {
+  if (items.length === 0) {
+    return <div>{"No questions yet :-("}</div>;
+  }
+
   return (
     <div>
       {items.map((item) => (
